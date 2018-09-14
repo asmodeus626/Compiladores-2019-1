108c4a252c79a3c5554938db04e882fd3e132bf9
DianaTadeo <36577863+DianaTadeo@users.noreply.github.com>
2018-09-14 14:44:39 -0500

    Lectura de archivos multiples

--- Proyectos/Proyecto_1/src/main/java/lexico/Test.java
+++ Proyectos/Proyecto_1/src/main/java/lexico/Test.java
@@ -3,7 +3,13 @@ package lexico;
 public class Test {
 
     public static void main (String[] args){
-        AnalizadorLexico al = new AnalizadorLexico("src/main/resources/error_identacion.py");
-        al.analiza();
+        AnalizadorLexico al1 = new AnalizadorLexico("src/main/resources/fizzbuzz.py");
+        al1.analiza();
+        AnalizadorLexico al2 = new AnalizadorLexico("src/main/resources/error_cadena.py");
+        al2.analiza();
+        AnalizadorLexico al3 = new AnalizadorLexico("src/main/resources/error_identacion.py");
+        al3.analiza();
+        AnalizadorLexico al4 = new AnalizadorLexico("src/main/resources/error_lexema.py");
+        al4.analiza();
     }
 }
