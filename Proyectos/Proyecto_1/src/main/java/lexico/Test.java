c059829eec70602856a57d331a87613f1bb56d9f
Rodrigo-Sanchez <rodrigosanchez@ciencias.unam.mx>
2018-09-13 22:26:21 -0500

    Quitando el import java.io.*;

--- Proyectos/Proyecto_1/src/main/java/lexico/Test.java
+++ Proyectos/Proyecto_1/src/main/java/lexico/Test.java
@@ -1,10 +1,9 @@
 package lexico;
-import java.io.*;
 
 public class Test {
 
     public static void main (String[] args){
-        AnalizadorLexico al = new AnalizadorLexico("src/main/resources/fb.txt");
+        AnalizadorLexico al = new AnalizadorLexico("src/main/resources/test.txt");
         al.analiza();
     }
 }
