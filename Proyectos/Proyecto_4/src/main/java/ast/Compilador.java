6097267b962c5b7b2fa8b572069251060a40947b
Rodrigo-Sanchez <rodrigosanchez@ciencias.unam.mx>
2018-11-19 21:55:09 -0600

    Agregando el último punto del proyecto.

--- Proyectos/Proyecto_4/src/main/java/ast/Compilador.java
+++ Proyectos/Proyecto_4/src/main/java/ast/Compilador.java
@@ -1,19 +1,19 @@
-
 package ast;
 import java.io.*;
 import ast.patron.compuesto.*;
 import ast.patron.visitante.*;
-
 
 public class Compilador{
 
     Parser parser;
     Nodo raízAST;
     VisitorPrint v_print;
+    VisitorType v_type;
 
     Compilador(Reader fuente){
-        parser = new Parser(fuente);
+        parser  = new Parser(fuente);
         v_print = new VisitorPrint();
+        v_type  = new VisitorType();
     }
 
     public void ConstruyeAST(boolean debug){
@@ -26,6 +26,10 @@ public class Compilador{
         parser.raiz.accept(v_print);
     }
 
+    public void verificaTiposAST(){
+        parser.raiz.accept(v_type);
+    }
+
     public static void main(String[] args){
             String archivo = "src/main/resources/test.txt";
         try{
@@ -33,6 +37,7 @@ public class Compilador{
             Compilador c  = new Compilador(a);
             c.ConstruyeAST(true);
             c.imprimeAST();
+            c.verificaTiposAST();
         }catch(FileNotFoundException e){
             System.err.println("El archivo " + archivo +" no fue encontrado. ");
         }catch(ArrayIndexOutOfBoundsException e){
