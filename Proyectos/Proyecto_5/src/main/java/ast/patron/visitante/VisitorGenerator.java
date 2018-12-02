f273cdc6ca93d56c7523c88601bd733ddad641ed
Rodrigo-Sanchez <rodrigosanchez@ciencias.unam.mx>
2018-12-02 13:55:45 -0600

    Actualizando VisitorGenerator.java que es un ejemplo de implementación para la generación de la operación de la resta.

--- Proyectos/Proyecto_5/src/main/java/ast/patron/visitante/VisitorGenerator.java
+++ Proyectos/Proyecto_5/src/main/java/ast/patron/visitante/VisitorGenerator.java
@@ -9,28 +9,26 @@ import ast.patron.compuesto.DifNodo;
 import ast.patron.compuesto.Nodo;
 import ast.patron.registros.Registros;
 
-// FALTA QUE IMPLEMENTE AL VISITANTE
 public class VisitorGenerator {
-  Registros reg = new Registros();
+    Registros reg = new Registros();
 
-  public void visit(DifNodo n){
-      Nodo hi = n.getPrimerHijo();
-      Nodo hd = n.getUltimoHijo();
+    public void visit(DifNodo n) {
+        Nodo hi = n.getPrimerHijo();
+        Nodo hd = n.getUltimoHijo();
 
-      //String objetivo = reg.getObjetivo();
-      String[] siguientes = reg.getNsiguientes(2);
+        String objetivo = reg.getObjetivo();
+        String[] siguientes = reg.getNsiguientes(2);
 
-      // Genero el código del subárbol izquiero
-      //reg.setObjetivo(siguiente[0]);
-      //hi.accept(this);
+        // Genero el código del subárbol izquierdo.
+        reg.setObjetivo(siguiente[0]);
+        hi.accept(this);
 
-      // Genero el código del subárbol derecho
-      //reg.setObjetivo(siguiente[1]);
-      //hd.accept(this);
+        // Genero el código del subárbol derecho
+        reg.setObjetivo(siguiente[1]);
+        hd.accept(this);
 
-      String opcdode =  "sub";
+        String opcdode = "sub";
 
-      //System.out.println(opcode + " " + objetivo + ", " +
-      //                    siguientes[0] + ", " + siguientes[1]);
-  }
+        System.out.println(opcode+" "+objetivo+", "+siguientes[0]+", "+siguientes[1]);
+    }
 }
